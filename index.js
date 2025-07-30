1ef5c9a81ac6de67c9317ce3c6b994512d2a529c
alvaro-ramirez-requena <alvaroramirez.requena@gmail.com>
2025-07-30 00:32:54 -0500

    cors added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import ss from './routes/index.js'
 import db from './configuration/db.js'
+import cors from 'cors'
 import './models/index.js'
+
 
 //Conexion a la base de datos
 
@@ -20,7 +22,14 @@ const app = express()
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 
-//  rutas de la app
+//Habilitar cors
+app.use(cors({
+    origin:['https://repository-alvaro-api-1-production.up.railway.app/productos'],
+    methods:['GET', 'PUT', 'POST', 'DELETE'],
+    credentials:true
+}))
+
+// rutas de la app
 app.use('/', ss)
 
 const port = process.env.PORT || 3001
