4b77cfe32dfa00bda69ce510a3223022e8850c39
alvaro-ramirez-requena <alvaroramirez.requena@gmail.com>
2025-08-11 01:01:22 -0500

    new-changes-errors-finish

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -2,45 +2,46 @@ import Usuario from "../models/Usuario.js"
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 
-const registrarUsuario = async(req, res) => {
+const registrarUsuario = async (req, res) => {
     const usuario = await Usuario.create(req.body)
     usuario.password = await bcrypt.hash(req.body.password, 12)
 
-    try{    
+    try {
         await usuario.save()
-        res.json({mensaje:"Usuario creado correctamente"})
-    }catch(error){
+        res.json({ mensaje: "Usuario creado correctamente" })
+    } catch (error) {
         console.log(error)
-        res.render({mensaje:'hubo un error'})
+        res.render({ mensaje: 'hubo un error' })
     }
 }
 
-const autenticarUsuario = async(req,res) => {
-    const {email, password} = req.body
-    const usuario = await Usuario.findOne({email})
-    
-    if(!usuario){
-        await res.status(401).json({mensaje: 'Ese usuario no existe'})
-    }else{
-        if(!bcrypt.compareSync(password, usuario.password)){
-            await res.status(401).json({mensaje: 'Password Incorrecto'})
-        }else{
+const autenticarUsuario = async (req, res) => {
+    const { email, password } = req.body
+    const usuario = await Usuario.findOne({ email })
+
+    if (usuario) {
+        if (!bcrypt.compareSync(password, usuario.password)) {
+            await res.status(401).json({ mensaje: 'Password Incorrecto' })
+        } else {
             const token = jwt.sign({
                 email: usuario.email,
                 nombre: usuario.nombre,
                 id: usuario.id
             },
-            'KEYSECRET',
-            {
-                expiresIn:'2h'
-            })
+                'KEYSECRET',
+                {
+                    expiresIn: '2h'
+                })
 
-            res.json({token})
+            res.json({ token })
         }
+    } else {
+        // await res.status(401).json({ mensaje: 'Ese usuario no existe' })
+        console.log('error')
     }
 }
 
-export{
+export {
     registrarUsuario,
     autenticarUsuario
 }
