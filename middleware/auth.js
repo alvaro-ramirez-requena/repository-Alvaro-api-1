4fe50129ab2fc02bfaedec5edfa0547a701c2905
alvaro-ramirez-requena <alvaroramirez.requena@gmail.com>
2025-08-12 13:11:23 -0500

    new-changes

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,9 @@
 import jwt from 'jsonwebtoken'
 
-const security =(req,res) => {
+const security =(req,res, next) => {
     const authHeader = req.get('Authorization')
     console.log(authHeader)
-    
+
     if(!authHeader){
         const error = new Error('No autenticado, no hay JWT')
         error.statusCode = 401
