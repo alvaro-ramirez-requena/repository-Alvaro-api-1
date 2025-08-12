5f29473cf2c0ff3e39e61827f77302db06e1e2db
alvaro-ramirez-requena <alvaroramirez.requena@gmail.com>
2025-08-11 22:32:29 -0500

    new-changes

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -15,30 +15,34 @@ const registrarUsuario = async (req, res) => {
 
 const autenticarUsuario = async (req, res) => {
     const { email, password } = req.body
-    const usuario = await Usuario.findOne({ 
-        where:{email}
-     })
-    
-    if (usuario) {
-        if (!bcrypt.compareSync(password, usuario.password)) {
-            await res.status(401).json({ mensaje: 'Password Incorrecto' })
-            // res.json({mensaje:"password incorrecto"})
-        } else {
-            const token = jwt.sign({
-                email: usuario.email,
-                nombre: usuario.nombre,
-                id: usuario.id
-            },
-                'KEYSECRET',
-                {
-                    expiresIn: '2h'
-                })
+    const usuario = await Usuario.findOne({
+        where: { email }
+    })
+    try {
+        if (usuario) {
+            if (!bcrypt.compareSync(password, usuario.password)) {
+                await res.status(401).json({ mensaje: 'Password Incorrecto' })
+                // res.json({mensaje:"password incorrecto"})
+            } else {
+                const token = jwt.sign({
+                    email: usuario.email,
+                    nombre: usuario.nombre,
+                    id: usuario.id
+                },
+                    'KEYSECRET',
+                    {
+                        expiresIn: '2h'
+                    })
 
-            res.json({ token })
+                res.json({ token })
+            }
+        } else {
+            await res.status(401).json({ mensaje: 'Ese usuario no existe' })
+            // res.json({mensaje:"Ese usuario no existe"})
         }
-    } else {
-        await res.status(401).json({ mensaje: 'Ese usuario no existe' })
-        // res.json({mensaje:"Ese usuario no existe"})
+
+    } catch (error) {
+        res.send(error)
     }
 }
 
