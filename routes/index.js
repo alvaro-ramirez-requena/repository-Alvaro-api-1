70488fc4a0885ba549bcb16fff3fa66248f2b4a1
alvaro-ramirez-requena <alvaroramirez.requena@gmail.com>
2025-08-13 02:38:59 -0500

    new-changes

--- routes/index.js
+++ routes/index.js
@@ -9,64 +9,64 @@ import security from '../middleware/auth.js'
 const router = express.Router()
 
 //Agrega nuevos clientes via POST
-router.post('/clientes', nuevoCliente)
+router.post('/clientes',security, nuevoCliente)
 
 //Obtener todos los clientes
 router.get('/clientes',security, mostrarClientes)
 
 //Muestra un cliente en especifico (id)
-router.get('/clientes/:id',  mostrarCliente )
+router.get('/clientes/:id', security, mostrarCliente )
 
 //Actualizar un cliente por su id
-router.put('/clientes/:id', actualizarCliente)
+router.put('/clientes/:id',security, actualizarCliente)
 
 //Eliminar un cliente por su id
-router.delete('/clientes/:id', eliminarCliente)
+router.delete('/clientes/:id',security, eliminarCliente)
 
 /* Productos */
 
 //Agrega nuevos productos
-router.post('/productos', 
+router.post('/productos', security,
     subirArchivo,
     nuevoProducto)
 
 //Mostrar todos los productos
-router.get('/productos', mostrarProductos)
+router.get('/productos', security,mostrarProductos)
 
 //Mostrar un producto por id
-router.get('/productos/:id', mostrarProducto)
+router.get('/productos/:id',security, mostrarProducto)
 
 //Actualizar producto por su id
-router.put('/productos/:id',subirArchivo,actualizarProducto)
+router.put('/productos/:id',security,subirArchivo,actualizarProducto)
 
 //Eliminar producto por su id
-router.delete('/productos/:id',eliminarProducto)
+router.delete('/productos/:id',security,eliminarProducto)
 
 //Busqueda de Productos
-router.get('/productos/busqueda/:query',buscarProducto)
+router.get('/productos/busqueda/:query',security,buscarProducto)
 
 /* Pedidos */
 
 
 //Agregar pedidos
-router.post('/pedidos', nuevoPedido)
+router.post('/pedidos', security,nuevoPedido)
 
 //Obtener pedidos
-router.get('/pedidos', obtenerPedidos)
+router.get('/pedidos', security,obtenerPedidos)
 
 //Mostrar pedido por Id
-router.get('/pedidos/:id', mostrarPedido)
+router.get('/pedidos/:id', security,mostrarPedido)
 
 //Actualizar el pedido por Id
-router.put('/pedidos/:id', actualizarPedido)
+router.put('/pedidos/:id', security,actualizarPedido)
 
 //Eliminar un pedido por su id
-router.delete('/pedidos/:id', eliminarPedido)
+router.delete('/pedidos/:id', security,eliminarPedido)
 
 //Usuarios
-router.post('/crear-cuenta', registrarUsuario)
+router.post('/crear-cuenta', security,registrarUsuario)
 
-router.post('/iniciar-sesion', autenticarUsuario)
+router.post('/iniciar-sesion', security,autenticarUsuario)
 
 
 
